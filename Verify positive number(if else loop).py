573eb269977fcce176eae34bc1a49530ba8936f0
Arjun M <90825849+Arjun8420@users.noreply.github.com>
2021-10-05 08:37:12 +0530

    Add files via upload

---
+++ Verify positive number(if else loop).py
@@ -0,0 +1,7 @@
+#Program to print the positive numbers using if else loop
+
+num = int(input("Enter the number = "))
+if num>=1 :
+    print(num,"is a positive number")
+else:
+    print(num,"is not a positive number")
